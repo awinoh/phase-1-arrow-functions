1640631d85289bbadf713e361ec0b7733ac36511
+ <awino.millicent@student.moringaschool.com>
2022-08-18 00:48:34 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(){
+ const divide=2000/100;
+ return divide; 
+}
+function square(){
+    let x = 2*2
+    return 4;
+}function add (){
+    let a =3
+    let b =4
+    return a+b
+}
